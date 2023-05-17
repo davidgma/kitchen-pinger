66ff8738c75b3084f8c0b8b5329a4becc876b7c3
David Ma <dmlh02@gmail.com>
2023-05-17 22:00:53 +0100

    moving to Input directives

--- src/app/footer-nav/footer-nav.component.ts
+++ src/app/footer-nav/footer-nav.component.ts
@@ -34,7 +34,7 @@ export class FooterNavComponent {
 
   editOnStackblitz(event: MouseEvent) {
     if (window != null) {
-      window.open('https://stackblitz.com/edit/css-clock', '_blank');
+      window.open('https://stackblitz.com/edit/kitchen-pinger', '_blank');
     }
   }
 }
